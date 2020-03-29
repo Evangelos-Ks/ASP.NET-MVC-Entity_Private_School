79bbec217627ec5fd10fa5b5604e8be97cac21c8
Evangelos Koutsogiorgos <www.evangelo@gmail.com>
2020-03-29 15:52:00 +0100

    Add class MyDatabase in Assignment2.Database

---
+++ Koutsogiorgos_Evangelos_Assignment_2/Assignment2.Database/MyDatabase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using Assignment2.Entities;
+
+namespace Assignment2.Database
+{
+    class MyDatabase : DbContext
+    {
+        public MyDatabase() : base("Connection") {}
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Trainer> Trainers { get; set; }
+        public DbSet<Assignment> Assignments { get; set; }
+    }
+}
