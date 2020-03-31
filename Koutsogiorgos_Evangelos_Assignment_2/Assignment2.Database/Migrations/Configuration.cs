20889f310707fab2646ddac51ae9d281eafe7a5b
Evangelos Koutsogiorgos <www.evangelo@gmail.com>
2020-03-31 15:00:02 +0100

    Seeding Courses

--- Koutsogiorgos_Evangelos_Assignment_2/Assignment2.Database/Migrations/Configuration.cs
+++ Koutsogiorgos_Evangelos_Assignment_2/Assignment2.Database/Migrations/Configuration.cs
@@ -17,11 +17,12 @@
         protected override void Seed(Assignment2.Database.MyDatabase context)
         {
             //==================== Seeding Courses ===========================================
-            Course c1 = new Course() { Title = "Physics" };
-            //Course c2 = new Course() { Title = "Chemistry" };
-            //Course c3 = new Course() { Title = "Biology" };
-            //Course c4 = new Course() { Title = "Mathematics" };
-            //Course c5 = new Course() { Title = "Information Technology" };
+            Course c1 = new Course() { Title = "Phisics", Stream = "Full time", Type = "Science" , StartDate = DateTime.Parse("2019/10/1"), EndDate = DateTime.Parse("2020/6/16") };
+            Course c2 = new Course() { Title = "Chemistry", Stream = "Full time", Type = "Science" , StartDate = DateTime.Parse("2019/10/1"), EndDate = DateTime.Parse("2020/6/11") };
+            Course c3 = new Course() { Title = "Biology", Stream = "Full time", Type = "Science" , StartDate = DateTime.Parse("2019/10/1"), EndDate = DateTime.Parse("2020/7/1") };
+            Course c4 = new Course() { Title = "Mathematics", Stream = "Full time", Type = "Science" , StartDate = DateTime.Parse("2019/10/1"), EndDate = DateTime.Parse("2020/5/30") };
+            Course c5 = new Course() { Title = "Information Technology", Stream = "Full time", Type = "Computer science" , StartDate = DateTime.Parse("2019/10/1"), EndDate = DateTime.Parse("2020/6/2") };
+
 
             //==================== Seeding Trainers ===========================================
             Trainer t1 = new Trainer() { FirstName = "Albert" };
@@ -56,26 +57,21 @@
             StudentCourse sc5 = new StudentCourse() { Course = c1, Student = s5 };
             StudentCourse sc6 = new StudentCourse() { Course = c1, Student = s6 };
 
+            //=================== Asign Assignments and Trainers to Courses ==========================
             c1.Assignments = new List<Assignment>() { a1, a2, a3 };
             c1.Trainers = new List<Trainer>() { t1, t2 };
 
+            //=================== Asign Courses to Trainers =========================================
             t1.Courses = new List<Course>() { c1 };
             t2.Courses = new List<Course>() { c1 };
 
-
-
-            //c1.StudentCourses = new List<StudentCourse>() { sc1, sc2, sc3, sc4, sc5, sc6 };
-
-            //t1.CourseTrainers = new List<CourseTrainer>() { ct1, ct2 };
-
+            //=================== Insert data to the database ========================================
             context.Assignments.AddOrUpdate(x => x.Title, a1, a2, a3);
-            context.Courses.AddOrUpdate(x => x.Title, c1);
+            context.Courses.AddOrUpdate(x => x.Title, c1, c2, c3, c4, c5);
             context.Students.AddOrUpdate(x => x.FirstName, s1, s2, s3, s4, s5, s6);
             context.Trainers.AddOrUpdate(x => x.FirstName, t1, t2);
             context.StudentsAssignments.AddOrUpdate(sa1, sa2, sa3, sa4, sa5, sa6);
             context.StudentsCourses.AddOrUpdate(x => new { x.CourseId, x.StudentId }, sc1, sc2, sc3, sc4, sc5, sc6);
-
-            
 
             context.SaveChanges();
         }
