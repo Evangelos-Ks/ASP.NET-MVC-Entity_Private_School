97fa592433609fcdc5ece581ed892568b1e6c8d8
Evangelos Koutsogiorgos <www.evangelo@gmail.com>
2020-04-05 20:03:26 +0100

    Add Students per course per assignment view

---
+++ Koutsogiorgos_Evangelos_Assignment_2/Assignment2.Web/Controllers/StudentsPerCoursePerAssignmentController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Assignment2.Services;
+
+namespace Assignment2.Web.Controllers
+{
+    public class StudentsPerCoursePerAssignmentController : Controller
+    {
+        // GET: StudentsPerCoursePerAssignment
+        public ActionResult AllStudentsPerCoursePerAssignment()
+        {
+            CourseRepository courseRepository = new CourseRepository();
+            var courses = courseRepository.GetAll();
+
+            return View(courses);
+        }
+    }
+}
