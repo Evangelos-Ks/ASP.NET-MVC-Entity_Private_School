b18223e6b69ff16a4349d046bb5bbfbc4462fcec
Evangelos Koutsogiorgos <evangkouts@gmail.com>
2020-06-16 20:03:39 +0100

    Added Register

---
+++ Koutsogiorgos_Evangelos_Assignment_2/Assignment2.Web/Register.aspx.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Assignment2.Web
+{
+    public partial class Register : System.Web.UI.Page
+    {
+        protected void CreateUser_Click(object sender, EventArgs e)
+        {
+            // Default UserStore constructor uses the default connection string named: DefaultConnection
+            var userStore = new UserStore<IdentityUser>();
+            var manager = new UserManager<IdentityUser>(userStore);
+
+            var user = new IdentityUser() { UserName = UserName.Text };
+            IdentityResult result = manager.Create(user, Password.Text);
+
+            if (result.Succeeded)
+            {
+                StatusMessage.Text = string.Format("User {0} was created successfully!", user.UserName);
+            }
+            else
+            {
+                StatusMessage.Text = result.Errors.FirstOrDefault();
+            }
+        }
+    }
+}
