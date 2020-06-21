9ba5e15ec8632a3af485e4b8375dfa046877fe29
Evangelos Koutsogiorgos <evangkouts@gmail.com>
2020-06-21 20:30:44 +0100

    Students per course per assignment sorting ready

--- Koutsogiorgos_Evangelos_Assignment_2/Assignment2.Web/Controllers/StudentsPerCoursePerAssignmentController.cs
+++ Koutsogiorgos_Evangelos_Assignment_2/Assignment2.Web/Controllers/StudentsPerCoursePerAssignmentController.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Mvc;
+﻿using System.Linq;
+using System.Web.Mvc;
 using Assignment2.Services;
 
 namespace Assignment2.Web.Controllers
@@ -6,12 +7,23 @@ namespace Assignment2.Web.Controllers
     public class StudentsPerCoursePerAssignmentController : Controller
     {
         // GET: StudentsPerCoursePerAssignment
-        public ActionResult AllStudentsPerCoursePerAssignment()
+        public ActionResult AllStudentsPerCoursePerAssignment(string sort)
         {
             CourseRepository courseRepository = new CourseRepository();
             var courses = courseRepository.GetAll();
             courseRepository.Dispose();
 
+            ViewBag.CourseTitle = string.IsNullOrEmpty(sort) ? "courseTitleDesc" : "";
+
+            switch (sort)
+            {
+                case "courseTitleDesc":
+                    courses = courses.OrderByDescending(x => x.Title);
+                    break;
+                default:
+                    courses = courses.OrderBy(x => x.Title);
+                    break;
+            }
             return View(courses);
         }
     }
