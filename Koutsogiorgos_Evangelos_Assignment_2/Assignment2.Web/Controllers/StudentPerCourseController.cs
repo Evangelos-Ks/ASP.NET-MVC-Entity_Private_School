4bef829991181ea892d36ae1446e2dde0bb4545f
Evangelos Koutsogiorgos <evangkouts@gmail.com>
2020-06-24 22:28:50 +0100

    Students per courses searching ready

--- Koutsogiorgos_Evangelos_Assignment_2/Assignment2.Web/Controllers/StudentPerCourseController.cs
+++ Koutsogiorgos_Evangelos_Assignment_2/Assignment2.Web/Controllers/StudentPerCourseController.cs
@@ -7,12 +7,20 @@ namespace Assignment2.Web.Controllers
     public class StudentPerCourseController : Controller
     {
         // GET: StudentCourse
-        public ActionResult AllStudentCourses(string sort)
+        public ActionResult AllStudentCourses(string sort, string search)
         {
             StudentCourseRepository studentCourseRepository = new StudentCourseRepository();
             var studentCourses = studentCourseRepository.GetAll();
             studentCourseRepository.Dispose();
 
+            //============================================== searching =====================================================
+            if (!string.IsNullOrEmpty(search))
+            {
+                studentCourses = studentCourses.Where(x => x.Course.Title.ToUpper().Contains(search.ToUpper()) ||
+                 x.Student.FirstName.ToUpper().Contains(search.ToUpper()) || x.Student.LastName.ToUpper().Contains(search.ToUpper()));
+            }
+
+            //============================================== sorting =======================================================
             ViewBag.CourseTitle = string.IsNullOrEmpty(sort) ? "courseTitleDesc" : "";
 
             switch (sort)
