18fabb312b79abaae250c9b8caa237e6a0fb0602
Evangelos Koutsogiorgos <evangkouts@gmail.com>
2020-06-25 20:31:15 +0100

    Assignments per course searching

--- Koutsogiorgos_Evangelos_Assignment_2/Assignment2.Web/Controllers/AssignmentsPerCourseController.cs
+++ Koutsogiorgos_Evangelos_Assignment_2/Assignment2.Web/Controllers/AssignmentsPerCourseController.cs
@@ -1,4 +1,6 @@
 ﻿using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 using System.Web.Mvc;
 using Assignment2.Services;
 
@@ -7,12 +9,19 @@ namespace Assignment2.Web.Controllers
     public class AssignmentsPerCourseController : Controller
     {
         // GET: AssignmentsPerCourse
-        public ActionResult AllAssignmentsPerCourse(string sort)
+        public ActionResult AllAssignmentsPerCourse(string sort, string search)
         {
             CourseRepository courseRepository = new CourseRepository();
             var courses = courseRepository.GetAll();
             courseRepository.Dispose();
 
+            //============================================== searching =====================================================
+            if (!string.IsNullOrEmpty(search))
+            {
+                courses = courses.Where(x => x.Title.ToUpper().Contains(search.ToUpper()));
+            }
+
+            //============================================== sorting =======================================================
             ViewBag.CourseTitle = string.IsNullOrEmpty(sort) ? "courseTitleDesc" : "";
 
             switch (sort)
