135293c10bff659582332aab79154db763be8898
Evangelos Koutsogiorgos <evangkouts@gmail.com>
2020-07-03 19:55:06 +0100

    Trainers per course pagination ready

--- Koutsogiorgos_Evangelos_Assignment_2/Assignment2.Web/Controllers/TrainersPerCourseController.cs
+++ Koutsogiorgos_Evangelos_Assignment_2/Assignment2.Web/Controllers/TrainersPerCourseController.cs
@@ -1,18 +1,53 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Runtime.Remoting.Metadata.W3cXsd2001;
 using System.Web.Mvc;
 using Assignment2.Services;
+using PagedList;
 
 namespace Assignment2.Web.Controllers
 {
     public class TrainersPerCourseController : Controller
     {
         // GET: TrainersPerCourse
-        public ActionResult AllTrainerCourse(string sort, string search)
+        public ActionResult AllTrainerCourse(string sort, string search, string currentFilter, int? page, int? pageSize, int? currentPageSize)
         {
             TrainerCourseRepository trainerCourseRepository = new TrainerCourseRepository();
             var trainerCourse = trainerCourseRepository.GetAll();
             trainerCourseRepository.Dispose();
+
+            //============================================== Paging ========================================================
+            int pSize;
+
+            if (search != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                search = currentFilter;
+            }
+
+            if (currentPageSize == null)
+            {
+                pSize = pageSize ?? 3;
+            }
+            else
+            {
+                pSize = pageSize ?? (int)currentPageSize;
+            }
+
+            int pageNumber = page ?? 1;
+
+            ViewBag.PageSize = new List<SelectListItem>()
+            {
+             new SelectListItem() { Value="3", Text= "3" },
+             new SelectListItem() { Value="5", Text= "5" },
+             new SelectListItem() { Value="10", Text= "10" },
+             new SelectListItem() { Value="15", Text= "15" },
+             new SelectListItem() { Value="25", Text= "25" },
+             new SelectListItem() { Value="50", Text= "50" }
+            };
 
             //============================================== searching =====================================================
             if (!string.IsNullOrEmpty(search))
@@ -33,7 +68,12 @@ namespace Assignment2.Web.Controllers
                     trainerCourse = trainerCourse.OrderBy(x => x.Course.Title);
                     break;
             }
-            return View(trainerCourse);
+
+            ViewBag.CurrentFilter = search;
+            ViewBag.CurrentSort = sort;
+            ViewBag.CurrentPageSize = pSize;
+
+            return View(trainerCourse.GroupBy(x => x.Course).ToPagedList(pageNumber, pSize));
         }
     }
 }
