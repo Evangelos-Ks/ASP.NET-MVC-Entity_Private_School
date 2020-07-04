02ecf7c6d8397ee44022fb699c307d04d992c72e
Evangelos Koutsogiorgos <evangkouts@gmail.com>
2020-07-04 16:52:37 +0100

    Assignments per course pagination ready

--- Koutsogiorgos_Evangelos_Assignment_2/Assignment2.Web/Controllers/AssignmentsPerCourseController.cs
+++ Koutsogiorgos_Evangelos_Assignment_2/Assignment2.Web/Controllers/AssignmentsPerCourseController.cs
@@ -1,19 +1,44 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Security.Cryptography.X509Certificates;
 using System.Web.Mvc;
 using Assignment2.Services;
+using PagedList;
 
 namespace Assignment2.Web.Controllers
 {
     public class AssignmentsPerCourseController : Controller
     {
         // GET: AssignmentsPerCourse
-        public ActionResult AllAssignmentsPerCourse(string sort, string search)
+        public ActionResult AllAssignmentsPerCourse(string sort, string search, string currentFilter, int? page, int? pageSize)
         {
             CourseRepository courseRepository = new CourseRepository();
             var courses = courseRepository.GetAll();
             courseRepository.Dispose();
+
+            //============================================== Paging ========================================================
+            if (!string.IsNullOrWhiteSpace(search) || search == "")
+            {
+                page = 1;
+            }
+            else
+            {
+                search = currentFilter;
+            }
+
+            int pSize = pageSize ?? 3;
+            int pageNumber = page ?? 1;
+
+            ViewBag.PageSize = new List<SelectListItem>()
+            {
+             new SelectListItem() { Value="3", Text= "3" },
+             new SelectListItem() { Value="5", Text= "5" },
+             new SelectListItem() { Value="10", Text= "10" },
+             new SelectListItem() { Value="15", Text= "15" },
+             new SelectListItem() { Value="25", Text= "25" },
+             new SelectListItem() { Value="50", Text= "50" }
+            };
 
             //============================================== searching =====================================================
             if (!string.IsNullOrEmpty(search))
@@ -34,7 +59,11 @@ namespace Assignment2.Web.Controllers
                     break;
             }
 
-            return View(courses);
+            ViewBag.CurrentFilter = search;
+            ViewBag.CurrentSort = sort;
+            ViewBag.CurrentPageSize = pSize;
+
+            return View(courses.GroupBy(x => x.Title).ToPagedList(pageNumber, pSize));
         }
     }
 }
