8b8b34f6c91eb10e976fede4dde35791f612360f
Evangelos Koutsogiorgos <evangkouts@gmail.com>
2020-07-07 19:53:59 +0300

    Added Statics ViewModel class (not ready)

---
+++ Koutsogiorgos_Evangelos_Assignment_2/Assignment2.Web/Models/StaticsViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Assignment2.Web.Models
+{
+    public class StaticsViewModel
+    {
+        protected int studentsCount { get; set; }
+    }
+}
