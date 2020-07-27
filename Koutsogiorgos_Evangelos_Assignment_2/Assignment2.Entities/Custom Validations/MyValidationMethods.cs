bceb3d32fad82e3edd3b705636b2b2022ec166dc
Evangelos Koutsogiorgos <evangkouts@gmail.com>
2020-07-27 21:51:31 +0100

    GraterThanDate validation not ready

--- Koutsogiorgos_Evangelos_Assignment_2/Assignment2.Entities/Custom Validations/MyValidationMethods.cs
+++ Koutsogiorgos_Evangelos_Assignment_2/Assignment2.Entities/Custom Validations/MyValidationMethods.cs
@@ -7,19 +7,38 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Assignment2.Entities.Custom_Validations
 {
-    public class MyValidationMethods : ValidationAttribute
+    public class GraterThanDate : ValidationAttribute
     {
-        //public static ValidationResult ValidateEndDateGreaterThanOtherStartDate(DateTime endDate, DateTime startDate, ValidationContext context)
-        //{
-        //    if (endDate > startDate)
-        //    {
-        //        return ValidationResult.Success;
-        //    }
-        //    else
-        //    {
-        //        return new ValidationResult("The end date can't be earlier than start date.");
-        //    }
+        //public DateTime? StartDate { get; set; }
 
+        //public GraterThanDate(DateTime? startDate)
+        //{
+        //    StartDate = startDate;
         //}
+
+        public ValidationResult ValidateEndDateGreaterThanOtherStartDate(DateTime endDate, ValidationContext context)
+        {
+            var startDateField = typeof(DateTime).GetProperty("StartDate");
+            //https://forums.asp.net/t/2088132.aspx?ASP+Net+MVC+Conditional+Validation+End+date+must+be+greater+than+or+equal+to+start+date
+            //var propertyValue = context.ObjectType.GetProperty("StartDate");
+            if (StartDate != null)
+            {
+
+                if (endDate > StartDate)
+                {
+                    return ValidationResult.Success;
+                }
+                else
+                {
+                    return new ValidationResult("The end date can't be earlier than start date.");
+                }
+            }
+            else
+            {
+                return ValidationResult.Success;
+            }
+
+
+        }
     }
 }
