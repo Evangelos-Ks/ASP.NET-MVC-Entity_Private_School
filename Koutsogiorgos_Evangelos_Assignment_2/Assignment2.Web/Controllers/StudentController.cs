a4d04105e0e2a84b087e3db4eed79b3e493f8270
Evangelos Koutsogiorgos <evangkouts@gmail.com>
2020-08-10 23:32:10 +0100

    Added control course for edit student mode

--- Koutsogiorgos_Evangelos_Assignment_2/Assignment2.Web/Controllers/StudentController.cs
+++ Koutsogiorgos_Evangelos_Assignment_2/Assignment2.Web/Controllers/StudentController.cs
@@ -8,6 +8,7 @@ using PagedList;
 using Assignment2.Web.Models;
 using System.IO;
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Assignment2.Web.Controllers
 {
@@ -121,6 +122,24 @@ namespace Assignment2.Web.Controllers
                 return HttpNotFound();
             }
 
+            //Seperate existing courses from the other courses
+            StudentCourseRepository studentCourseRepository = new StudentCourseRepository();
+            IEnumerable<StudentCourse> studentCourses = studentCourseRepository.GetAll().Where(sc => sc.StudentId == id);
+            studentCourseRepository.Dispose();
+            IEnumerable<int> existingCoursesId = studentCourses.Select(sc => sc.CourseId);
+
+            CourseRepository courseRepository = new CourseRepository();
+            List<Course> allCourses = courseRepository.GetAll().ToList();
+            courseRepository.Dispose();
+
+            List<Course> existingCourses = new List<Course>();
+            foreach (int existId in existingCoursesId)
+            {
+                Course course = allCourses.FirstOrDefault(x => x.CourseId == existId);
+                existingCourses.Add(course);
+                allCourses.Remove(course);
+            }
+
             //Create studentViewModel
             StudentViewModel studentViewModel = new StudentViewModel()
             {
@@ -129,7 +148,9 @@ namespace Assignment2.Web.Controllers
                 LastName = student.LastName,
                 DateOfBirth = student.DateOfBirth,
                 Discount = student.Discount,
-                PhotoUrl = student.PhotoUrl
+                PhotoUrl = student.PhotoUrl,
+                CoursesForAddition = CreateSelectListOfCourses(allCourses),
+                ExistingCourses = CreateSelectListOfCourses(existingCourses)
             };
 
             return View(studentViewModel);
@@ -140,7 +161,7 @@ namespace Assignment2.Web.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult EditStudent([Bind(Include = "StudentId,FirstName,LastName,DateOfBirth,ImageFile")] StudentViewModel studentViewModel)
+        public ActionResult EditStudent([Bind(Include = "StudentId,FirstName,LastName,DateOfBirth,ImageFile,Discount,PhotoUrl,ExistingCoursesId,CoursesForAdditionId")] StudentViewModel studentViewModel)
         {
             if (ModelState.IsValid)
             {
@@ -152,6 +173,34 @@ namespace Assignment2.Web.Controllers
                     studentViewModel.PhotoUrl = "../../Content/Students_Image/" + fileName;
                     fileName = Path.Combine(Server.MapPath("~/Content/Students_Image/"), fileName);
                     studentViewModel.ImageFile.SaveAs(fileName);
+                }
+
+                //Update StudentCourse
+                if (studentViewModel.ExistingCoursesId != null || studentViewModel.CoursesForAdditionId != null)
+                {
+                    StudentCourseRepository studentCourseRepository = new StudentCourseRepository();
+                    IEnumerable<StudentCourse> studentCourses = studentCourseRepository.GetAll().Where(sc => sc.StudentId == studentViewModel.StudentId);
+                    List<StudentCourse> existingStudentCourses = studentCourses.Where(sc => sc.StudentId == studentViewModel.StudentId).ToList();
+                    if (studentViewModel.ExistingCoursesId != null)
+                    {
+                        foreach (string id in studentViewModel.ExistingCoursesId)
+                        {
+                            studentCourseRepository.Delete(existingStudentCourses.Find(sc => sc.CourseId == Convert.ToInt32(id)));
+                        }
+                    }
+                    if (studentViewModel.CoursesForAdditionId != null)
+                    {
+                        foreach (string id in studentViewModel.CoursesForAdditionId)
+                        {
+                            StudentCourse studentCourse = new StudentCourse()
+                            {
+                                StudentId = studentViewModel.StudentId,
+                                CourseId = Convert.ToInt32(id)
+                            };
+                            studentCourseRepository.Insert(studentCourse);
+                        }
+                    }
+                    studentCourseRepository.Dispose();
                 }
 
                 //Update student
@@ -288,6 +337,15 @@ namespace Assignment2.Web.Controllers
                 studentCourseRepository.Delete(studentCourse);
             }
             studentCourseRepository.Dispose();
+
+            //Delete studentAssignments
+            StudentAssignmentRepository studentAssignmentRepository = new StudentAssignmentRepository();
+            List<StudentAssignment> studentAssignments = studentAssignmentRepository.GetAll().Where(sa => sa.StudentId == id).ToList();
+            foreach (StudentAssignment studentAssignment in studentAssignments)
+            {
+                studentAssignmentRepository.Delete(studentAssignment);
+            }
+            studentAssignmentRepository.Dispose();
 
             //Delete student
             StudentRepository studentRepository = new StudentRepository();
