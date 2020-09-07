dce0a0e0793b6c75d385975051fc43ad5086b737
Evangelos Koutsogiorgos <evangkouts@gmail.com>
2020-09-07 13:28:11 +0100

    How-to-Guides in courses ready

--- Koutsogiorgos_Evangelos_Assignment_2/Assignment2.Web/Controllers/HomeController.cs
+++ Koutsogiorgos_Evangelos_Assignment_2/Assignment2.Web/Controllers/HomeController.cs
@@ -9,17 +9,8 @@ namespace Assignment2.Web.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult AboutCourses()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
